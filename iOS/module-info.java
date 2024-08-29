3d67ab1a2ef0c026c48b4619acbfc5b8eaa5a889
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2024-08-29 15:46:48 +0200

    Delete iOS directory
    Signed-off-by: FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>

--- iOS/module-info.java
+++
@@ -1,8 +0,0 @@
-module freetimemaker.github.io {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    opens freetimemaker.github.io to javafx.fxml;
-    exports freetimemaker.github.io;
-}
