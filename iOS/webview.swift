bda955b61b2334339a2a2472416a3d01c5750b93
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2024-08-29 09:34:48 +0200

    Create webview.swift
    Signed-off-by: FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>

---
+++ iOS/webview.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import WebKit
+
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://freetimemaker.github.io")!)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        uiView.load(request)
+    }
+}
