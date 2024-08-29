462caa2646f09302c734334d0932590f358c36f3
FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>
2024-08-29 10:28:46 +0200

    Create webview_page.dart
    Signed-off-by: FreetimeMaker <165287054+FreetimeMaker@users.noreply.github.com>

---
+++ iOS/webview_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebView Example'),
+      ),
+      body: WebView(
+        initialUrl: 'https://freetimemaker.github.io',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
